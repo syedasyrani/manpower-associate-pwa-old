c9478cfe35e8ecdcc35042c7ae06de7244a87764
Syed Asyrani <syedasyrani@gmail.com>
2019-07-15 11:07:02 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Layout from 'Layout'
+import Login from 'Login'
+
+
+class App extends Component {
+  state = {
+    isLoggedIn: false
+  }
+  render() { 
+    return this.state.sLoggedIn
+      ? <Layout />
+      : <Login />
+  }
 }
 
-export default App;
+export default App
---
+++ src/containers/Login/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const login = () => (
+    <div>
+        
+    </div>
+)
+
+export default login
