93d3d340986e63864f311a06315a22d4511e8e6c
Syed Asyrani <syedasyrani@gmail.com>
2019-07-15 14:26:23 +0800

    updated navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,8 +3,18 @@ import styles from './Navbar.css'
 
 class Navbar extends Component {
     render() { 
-        return <nav className={styles.Navbar + " navbar navbar-light bg-light"}>
-        <span className="navbar-brand mb-0 h1">Navbar</span>
+        return <nav className={styles.Navbar + " navbar bg-white align-center border-bottom"}>
+          <i className="fas fa-bars"></i>
+          <h1 className="navbar-brand mb-0">Amazon <span className="pl-1" style={{color: '#aaa'}}>USG1</span></h1>
+          
+          <span className="navbar-text d-flex align-center">
+            <i className="far fa-bell px-3" style={{color: '#aaa',fontSize: '1.5rem'}}></i>
+            <img 
+              src="https://t3.ftcdn.net/jpg/00/64/67/80/240_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg" 
+              alt="default"
+              className="rounded-circle border"
+              style={{height: '25px', width: '25px'}} />
+          </span>
       </nav>
     }
 }
